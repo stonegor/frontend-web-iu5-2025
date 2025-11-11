9559986ebcfc6da1f45534e67e452dd455f1566b
geoheat.ed_vkusvill.ru <sstonegor@gmail.com>
2025-11-11 17:50:13 +0300

    feat: match prediction page to ssg

--- src/pages/AuthorDetailPage.tsx
+++ src/pages/AuthorDetailPage.tsx
@@ -1,5 +1,59 @@
-import type { FC } from "react";
+import { type FC } from "react";
+import { useParams } from "react-router-dom";
+import { AUTHORS_MOCK } from "../api/mock";
+import { User } from "lucide-react";
 
 export const AuthorDetailPage: FC = () => {
-  return <div>AuthorDetailPage</div>;
+  const { id } = useParams<{ id: string }>();
+  const author = AUTHORS_MOCK.find((a) => a.id === Number(id));
+
+  if (!author) {
+    return <div>Автор не найден</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="page-title">
+        <User strokeWidth={3} />
+        <span>{author.name}</span>
+      </h1>
+
+      <div className="nutrient is-promoted card usa-card__container clearfix author-card-huge">
+        <div className="usa-card__media">
+          <img
+            src={author.image_url}
+            alt={author.name}
+            className="card-image"
+          />
+        </div>
+        <div className="card-content">
+          <div className="column-large card-column">
+            <div className="author-about">
+              <h2>Об авторе</h2>
+              <p>{author.about || "Нет информации об авторе."}</p>
+            </div>
+          </div>
+          <div className="column-small card-column">
+            <div className="author-stats">
+              <h2 className="card-subtitle">В среднем на 100 слов:</h2>
+              <div className="stats-grid">
+                <div className="column">
+                  <p>в: {author.count_v}</p>
+                  <p>и: {author.count_i}</p>
+                  <p>но: {author.count_no}</p>
+                  <p>ли: {author.count_li}</p>
+                </div>
+                <div className="column">
+                  <p>да: {author.count_da}</p>
+                  <p>же: {author.count_zhe}</p>
+                  <p>или: {author.count_ili}</p>
+                  <p>либо: {author.count_libo}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/PredictionPage.tsx
+++ src/pages/PredictionPage.tsx
@@ -1,5 +1,66 @@
-import type { FC } from "react";
+import { type FC } from "react";
+import { UserSearch } from "lucide-react";
+import { AUTHORS_MOCK } from "../api/mock";
 
 export const PredictionPage: FC = () => {
-  return <div>PredictionPage</div>;
+  const authors = AUTHORS_MOCK;
+
+  return (
+    <div className="prediction-detail-container">
+      <h1 className="page-title">
+        <UserSearch strokeWidth={3} />
+        <span>Поиск Автора по тексту</span>
+      </h1>
+
+      <div className="search-and-summary">
+        <form action="" method="get" className="search-form prediction-form">
+          <textarea
+            name="author_name"
+            placeholder="Введите текст для анализа"
+          ></textarea>
+        </form>
+      </div>
+
+      <h2 className="authors-list-header">Результаты</h2>
+      <div className="results-cards">
+        {authors.map((author) => (
+          <div className="card" key={author.id}>
+            <div className="card-image-container">
+              <img
+                src={author.image_url}
+                alt={author.name}
+                className="card-image"
+              />
+            </div>
+            <div className="card-main-content">
+              <h3 className="card-title">{author.name}</h3>
+              <div className="card-details">
+                <p className="card-stats">
+                  в: {author.count_v}, и: {author.count_i}, не:{" "}
+                  {author.count_no}, ли: {author.count_li}, да:{" "}
+                  {author.count_da}, же: {author.count_zhe}, или:{" "}
+                  {author.count_ili}, либо: {author.count_libo}
+                </p>
+                <div className="dropdown">
+                  <label htmlFor={`period-${author.id}`}>Период:</label>
+                  <select name="period" id={`period-${author.id}`}>
+                    <option value="early">Ранний</option>
+                    <option value="mature">Зрелый</option>
+                    <option value="late">Поздний</option>
+                  </select>
+                </div>
+                <div className="probability-display">Вероятность: 0.5</div>
+              </div>
+            </div>
+            <div className="card-actions"></div>
+          </div>
+        ))}
+      </div>
+      <form method="post" action="">
+        <button type="submit" className="delete-button">
+          Удалить
+        </button>
+      </form>
+    </div>
+  );
 };
