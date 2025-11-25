c7e5bbe9a1c6dbdb4b55cafc22022d447b7e8c36
geoheat.ed_vkusvill.ru <sstonegor@gmail.com>
2025-11-25 22:22:51 +0300

    chore: update local network ip configuration

--- src/target_config.ts
+++ src/target_config.ts
@@ -1,6 +1,7 @@
-export const target_tauri = true;
+export const target_tauri = true; // Set to true for Tauri build, false for Web/PWA
 
-export const api_proxy_addr = "http://192.168.1.100:8000";
+// Update this IP to your local network IP of the backend
+export const api_proxy_addr = "http://192.168.1.2:8000"; 
 
 export const dest_api = target_tauri ? api_proxy_addr : "/api";
 export const dest_root = target_tauri ? "/" : "/frontend-web-iu5-2025/";
