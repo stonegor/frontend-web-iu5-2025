baa4d2ad9a39d9934d4589972e957c9724149c16
geoheat.ed_vkusvill.ru <sstonegor@gmail.com>
2025-11-26 13:25:31 +0300

    fix: backend base url

--- src/api/authors.ts
+++ src/api/authors.ts
@@ -1,9 +1,10 @@
 import type { Author } from './types'
+import { API_BASE_URL } from '../config'
 import { AUTHORS_MOCK } from './mock'
 
 export const getAuthors = async (): Promise<Author[]> => {
   try {
-    const response = await fetch('/api/authors')
+    const response = await fetch(`${API_BASE_URL}/api/authors`)
     if (!response.ok) {
       throw new Error('Network response was not ok')
     }
@@ -16,7 +17,7 @@ export const getAuthors = async (): Promise<Author[]> => {
 
 export const getAuthorById = async (id: number): Promise<Author | undefined> => {
   try {
-    const response = await fetch(`/api/authors/${id}`)
+    const response = await fetch(`${API_BASE_URL}/api/authors/${id}`)
     if (!response.ok) {
       throw new Error('Network response was not ok')
     }
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ }) => ({
+export default defineConfig(() => ({
   base: '/frontend-web-iu5-2025/',
   plugins: [
     react(),
